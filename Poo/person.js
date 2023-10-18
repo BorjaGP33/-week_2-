d1b4c9914da3334526db51dd82158372b7d02fed
BorjaGP33 <borja1131987@gmail.com>
2023-10-18 17:33:05 +0200

    subida clase person

---
+++ Poo/person.js
@@ -0,0 +1,53 @@
+class person {
+    constructor(nombre, peso, altura, yearOfBirth) {
+
+        this.nombre = nombre
+        this.peso = peso
+        this.altura = altura
+        this.yearOfBirth = yearOfBirth
+        this.Hobbies = []
+    }
+
+
+    //  ______ reto-2
+
+
+    calcularIMC() {
+        return this.peso / (this.altura * this.altura) * 10000
+    }
+
+    printAll() {
+        console.log(`su nombre es ${this.nombre} - su peso es: ${this.peso} - su altura es: ${this.altura} - su fecha es: ${this.yearOfBirth}`)
+    };
+
+    printhobbies() {
+        console.log(`sus hobbies es ${this.Hobbies}`)
+    };
+
+    calculaEdad(yearOfBirth) {
+        let year = new Date().getFullYear();
+        return this.year - yearOfBirth;
+    }
+
+
+
+}
+let persona = new person("Jose", 75, 175, 1963);
+//     console.log(persona);
+
+persona.Hobbies = ["nadar", "correr", "jugar"];
+
+//     console.log(persona.calcularIMC());
+persona.printAll()
+persona.printhobbies()
+
+// reto 3
+
+module.exports = { person }
+
+
+
+
+
+
+
---
+++ Poo/personTest.js
@@ -0,0 +1,12 @@
+const {person} = require ("./person");
+
+let persona = new person("Jose", 75, 175, 1963);
+let persona2 = new person("paz", 78, 163, 1962);
+
+persona2.Hobbies = ["peliculas","bailar","verTv"];
+
+persona.printAll()
+persona.printhobbies()
+
+persona2.printAll()
+persona.printhobbies()
